e59fedc6aedc7c5303e891fdacf7b1793f740362
dantesantos <46685889+dantesantos@users.noreply.github.com>
2019-05-23 17:26:00 -0400

    Any messages

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ app.get('/about', (req,res) =>{
     res.send(`I like long walks on the beach.`);
 });
 
-app.listen(4000, () => console.log('Listening on 4000'));
+app.listen((process.env.PORT || 4000), () => console.log('Listening on 4000'));
